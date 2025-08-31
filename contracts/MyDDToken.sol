0d85d4706ce294f359dffcd106b60aa69eb76185
li-qiushuang <lili2lucy@gmail.com>
2025-08-31 10:04:13 +0800

    feat: 更新token中的Decimals没有处理问题

--- contracts/MyDDToken.sol
+++ contracts/MyDDToken.sol
@@ -6,10 +6,10 @@ pragma solidity ^0.8.28;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // 定义我们的代币合约，它继承自 OpenZeppelin 的 ERC20 合约
-contract MyToken is ERC20 {
-    string public constant TOKENNAME = "WWERC20TOKEN";
-    string public constant TOKENYMBOL = "WWB";
-    uint256 public constant TOKEN_INITSUPPLY = 1000;
+contract MyDDToken is ERC20 {
+    string public constant TOKENNAME = "DangDangTOKEN";
+    string public constant TOKENYMBOL = "DD";
+    uint256 public constant TOKEN_INITSUPPLY = 1000 * 10 ** 18;
 
     constructor() ERC20(TOKENNAME, TOKENYMBOL) {
         // 将初始供应量的代币铸造给部署合约的人（msg.sender）
--- ignition/modules/MyToken.js
+++
@@ -1,6 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("MyToken", (m) => {
-        const myToken = m.contract("MyToken");
-        return { myToken };
-});
